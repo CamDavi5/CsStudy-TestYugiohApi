4bdb9f28858466b608cf220bb28f857bb7fab993
CamDavi5 <camrdavis.biz@gmail.com>
2022-07-12 15:37:01 -0500

    Add project files.

---
+++ TestYugiohApi/Models/YugiohCard.cs
@@ -0,0 +1,11 @@
+﻿namespace TestYugiohApi.Models
+{
+    public class YugiohCard
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Level { get; set; }
+            public string Attribute { get; set; }
+            public string Type { get; set; }
+    }
+}
---
+++ TestYugiohApi/Models/YugiohContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestYugiohApi.Models
+{
+    public class YugiohContext : DbContext
+    {
+        public YugiohContext(DbContextOptions<YugiohContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<YugiohCard> YugiohCards { get; set; }
+    }
+}
